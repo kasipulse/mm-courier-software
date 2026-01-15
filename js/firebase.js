f7153a1132092e9cf8e2d7eaf3b56af65a9e8b67
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-01-15 11:43:56 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,20 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getFirestore, collection, doc, setDoc, writeBatch } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
 
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDVn4lqyV-I9Z1tQNKnrbHEZCvdNY1FDY4",
+  authDomain: "mm-courier-software.firebaseapp.com",
+  projectId: "mm-courier-software",
+  storageBucket: "mm-courier-software.firebasestorage.app",
+  messagingSenderId: "1088997998974",
+  appId: "1:1088997998974:web:7d31deb10940ef2b4af32f",
+  measurementId: "G-S9MW03EQ5C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
